e831545c71a15dc89414cda5d0636ad6af233df6
Arman Rostami <arman.rostami.999@gmail.com>
2019-12-07 02:49:39 +0330

    Add exceptions

---
+++ exceptions.hpp
@@ -0,0 +1,24 @@
+#ifndef __EXCEPTIONS_H__
+#define __EXCEPTIONS_H__
+
+#include "exception.hpp"
+
+class Not_Found_Exception : public Exception 
+{
+public:
+    Not_Found_Exception() : Exception("Not Found") {}
+};
+
+class Bad_Request_Exception : public Exception 
+{
+public:
+    Bad_Request_Exception() : Exception("Bad Request") {}
+};
+
+class Permission_Exception : public Exception 
+{
+public:
+    Permission_Exception() : Exception("Permission Denied") {}
+};
+
+#endif
