4574b0e73ce539e0b0dbaf6703c4afc9aa701298
zhpinkman <zhivarsourati@gmail.com>
2019-12-06 11:15:53 +0330

    tools added

---
+++ tools.hpp
@@ -0,0 +1,13 @@
+#include <vector>
+#include <string>
+#include <fstream>
+
+class Tools
+{
+private:
+public:
+    Tools();
+    std::fstream openCSVFile(std::string fileName);
+    std::vector<std::string> parseCSVFile(std::fstream file);
+    std::vector<std::string> splitBySpace(std::string spacedString);
+};
