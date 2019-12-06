dab029da78817d442d33d29a2175ac523fc7885a
zhpinkman <zhivarsourati@gmail.com>
2019-12-06 10:59:38 +0330

    buyer added

---
+++ buyer.hpp
@@ -0,0 +1,24 @@
+#include "user.hpp"
+#include "wallet.hpp"
+#include "offer.hpp"
+#include "factor.hpp"
+
+class CartItem;
+
+class Buyer : public User
+{
+private:
+    Wallet *wallet;
+    std::vector<CartItem *> cart;
+
+public:
+    Buyer();
+    bool addToCart(Offer *offer, int amout, std::string discountCode);
+    bool submitCart();
+    std::vector<std::string> getOrdersHistory(int bound);
+    std::vector<std::string> getTransactionHistory(int bound);
+    bool chargeWallet(double amount);
+    std::vector<std::string> getCoupons();
+    bool payFactor(Factor *factor);
+    bool checkAuthorization(std::string endpointID);
+};
