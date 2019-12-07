0b7b0f9aea26b313e119bb2f7eae8494a04e7c3f
Arman Rostami <arman.rostami.999@gmail.com>
2019-12-07 03:48:56 +0330

    Add splitBySpace function

---
+++ Tools.cpp
@@ -0,0 +1,15 @@
+#include "tools.hpp"
+#include <sstream>
+using namespace std;
+
+vector<string> Tools::splitBySpace(string spacedString)
+{
+    vector<string> words;
+    istringstream wordsStream(spacedString);
+
+    string word;
+    while(wordsStream >> word)
+        words.push_back(word);
+
+    return words;
+}
---
+++ Tools.hpp
@@ -0,0 +1,13 @@
+#ifndef _TOOLS_H_
+#define _TOOLS_H_
+
+#include <vector>
+#include <string>
+
+class Tools
+{
+public:
+    static std::vector<std::string> splitBySpace(std::string spacedString);
+};
+
+#endif
