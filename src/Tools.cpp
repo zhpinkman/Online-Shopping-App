d84b5ff6d251a3bd2beb278893389ec9a22ed145
zhpinkman <zhivarsourati@gmail.com>
2019-12-08 10:39:09 +0330

    tools implemented methods added

--- src/Tools.cpp
+++ src/Tools.cpp
@@ -1,15 +1,35 @@
 #include "Tools.hpp"
 #include <sstream>
+#include <fstream>
+#include <random>
+#include <algorithm>
 using namespace std;
 
+Tools::Tools()
+{
+}
 vector<string> Tools::splitBySpace(string spacedString)
 {
     vector<string> words;
     istringstream wordsStream(spacedString);
 
     string word;
-    while(wordsStream >> word)
+    while (wordsStream >> word)
         words.push_back(word);
 
     return words;
+}
+
+std::fstream Tools::openCSVFile(std::string fileName) {}
+std::vector<std::string> Tools::parseCSVFile(std::fstream file) {}
+std::string Tools::generateRandomString()
+{
+
+    std::string str("0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz");
+    std::random_device rd;
+    std::mt19937 generator(rd());
+
+    std::shuffle(str.begin(), str.end(), generator);
+
+    return str.substr(0, 32); // assumes 32 < number of characters in str
 }
--- src/Tools.hpp
+++ src/Tools.hpp
@@ -7,8 +7,13 @@
 
 class Tools
 {
+private:
 public:
+    Tools();
+    std::fstream openCSVFile(std::string fileName);
+    std::vector<std::string> parseCSVFile(std::fstream file);
     static std::vector<std::string> splitBySpace(std::string spacedString);
+    static std::string generateRandomString();
 };
 
 #endif
