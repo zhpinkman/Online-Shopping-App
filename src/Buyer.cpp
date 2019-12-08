4cf1ba65ee588d6fa5b5b0441aab659f77867d67
zhpinkman <zhivarsourati@gmail.com>
2019-12-08 19:13:21 +0330

    buyer implemented methods added

--- src/Buyer.cpp
+++ src/Buyer.cpp
@@ -7,60 +7,60 @@ Buyer::Buyer(API *api, int userId, int walletId, std::string username, std::stri
 {
 }
 
-// bool Buyer::addToCart(Offer *offer, int amount, std::string discountCode = "")
-// {
-//     if ((api->isValidDiscountCode(offer, discountCode) || discountCode == "") && api->canBeAddedToCart(offer, amount))
-//     {
-//         int discountPercentage;
-//         if (discountCode != "")
-//             discountPercentage = api->useDiscountCode(discountCode);
-//         discountPercentage = 0;
-//         CartItem *cartItem = new CartItem(offer, amount, discountPercentage);
-//         std::cout << OK << std::endl;
-//         return SUCCESS;
-//     }
-//     else
-//     {
-//         std::cout << BAD_REQUEST << std::endl;
-//         return FAILED;
-//     }
-// }
-// bool Buyer::submitCart()
-// {
-//     Factor *factor = new Factor(cart);
-//     double finalPrice = factor->getFinalPrice();
-//     if (wallet->withdraw(finalPrice))
-//     {
-//         std::cout << OK << std::endl;
-//         return SUCCESS;
-//     }
-//     else
-//     {
-//         std::cout << BAD_REQUEST << std::endl;
-//         return FAILED;
-//     }
-// }
-// std::vector<std::string> Buyer::getOrdersHistory(int bound)
-// {
-//     for (int i = 0; i < orders.size(); i++)
-//     {
-//         std::cout << orders[i].first->getProductId() << SEPERATOR << orders[i].first->getProductName() << SEPERATOR;
-//         std::cout << orders[i].first->getId() << SEPERATOR << orders[i].first->getSellerId() << SEPERATOR;
-//         std::cout << orders[i].second->getSoldPrice() << std::endl;
-//     }
-// }
-// std::vector<std::string> Buyer::getTransactionHistory(int bound)
-// {
-//     std::vector<double> transactionHistory = wallet->getHistory(bound);
-//     for (double transaction : transactionHistory)
-//     {
-//         std::cout << transaction << std::endl;
-//     }
-// }
-// bool Buyer::chargeWallet(double amount)
-// {
-//     wallet->charge(amount);
-// }
-// std::vector<std::string> Buyer::getCoupons() {}
-// bool Buyer::payFactor(Factor *factor) {}
-// bool Buyer::checkAuthorization(std::string endpointID) {}
+bool Buyer::addToCart(Offer *offer, int amount, std::string discountCode = "")
+{
+    if ((api->isValidDiscountCode(offer, discountCode) || discountCode == "") && api->canBeAddedToCart(offer, amount))
+    {
+        int discountPercentage;
+        if (discountCode != "")
+            discountPercentage = api->useDiscountCode(discountCode);
+        discountPercentage = 0;
+        CartItem *cartItem = new CartItem(offer, amount, discountPercentage);
+        std::cout << OK << std::endl;
+        return SUCCESS;
+    }
+    else
+    {
+        std::cout << BAD_REQUEST << std::endl;
+        return FAILED;
+    }
+}
+bool Buyer::submitCart()
+{
+    Factor *factor = new Factor(cart);
+    double finalPrice = factor->getFinalPrice();
+    if (wallet->withdraw(finalPrice))
+    {
+        std::cout << OK << std::endl;
+        return SUCCESS;
+    }
+    else
+    {
+        std::cout << BAD_REQUEST << std::endl;
+        return FAILED;
+    }
+}
+std::vector<std::string> Buyer::getOrdersHistory(int bound)
+{
+    for (int i = 0; i < orders.size(); i++)
+    {
+        std::cout << orders[i].first->getProductId() << SEPERATOR << orders[i].first->getProductName() << SEPERATOR;
+        std::cout << orders[i].first->getId() << SEPERATOR << orders[i].first->getSellerId() << SEPERATOR;
+        std::cout << orders[i].second->getSoldPrice() << std::endl;
+    }
+}
+std::vector<std::string> Buyer::getTransactionHistory(int bound)
+{
+    std::vector<double> transactionHistory = wallet->getHistory(bound);
+    for (double transaction : transactionHistory)
+    {
+        std::cout << transaction << std::endl;
+    }
+}
+bool Buyer::chargeWallet(double amount)
+{
+    wallet->charge(amount);
+}
+std::vector<std::string> Buyer::getCoupons() {}
+bool Buyer::payFactor(Factor *factor) {}
+bool Buyer::checkAuthorization(std::string endpointID) {}
--- src/Buyer.hpp
+++ src/Buyer.hpp
@@ -13,19 +13,19 @@ class Buyer : public User
 {
 public:
     Buyer(API *api, int userId, int walletId, std::string username, std::string email, std::string password);
-    // bool addToCart(Offer *offer, int amount, std::string discountCode);
-    // bool submitCart();
-    // std::vector<std::string> getOrdersHistory(int bound);
-    // std::vector<std::string> getTransactionHistory(int bound);
-    // bool chargeWallet(double amount);
-    // std::vector<std::string> getCoupons();
-    // bool payFactor(Factor *factor);
-    // bool checkAuthorization(std::string endpointID);
+    bool addToCart(Offer *offer, int amount, std::string discountCode);
+    bool submitCart();
+    std::vector<std::string> getOrdersHistory(int bound);
+    std::vector<std::string> getTransactionHistory(int bound);
+    bool chargeWallet(double amount);
+    std::vector<std::string> getCoupons();
+    bool payFactor(Factor *factor);
+    bool checkAuthorization(std::string endpointID);
 
 private:
     Wallet *wallet;
-    // std::vector<CartItem *> cart;
-    // std::vector<std::pair<Offer*, CartItem*>> orders;
+    std::vector<CartItem *> cart;
+    std::vector<std::pair<Offer *, CartItem *>> orders;
 };
 
 #endif
